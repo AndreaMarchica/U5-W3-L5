56c30d9d33268d3d8dcfee99e3d4d2010a6f38ad
AndreaMarchica <andrea.marchica94@gmail.com>
2024-01-26 10:28:18 +0100

    create project folder

---
+++ src/test/java/andreamarchica/U5W3L5/U5W3L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package andreamarchica.U5W3L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
